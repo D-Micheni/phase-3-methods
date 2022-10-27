e8580bb1dee7bc933f64084100a2c9a875102be5
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-10-27 19:33:15 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  def greet name
+    puts "Hello, #{name}!"
+  end
+  
+  def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+  end
+  
+  def add (num1,num2)
+    num1 + num2
+  end
+  
+  def halve int
+    return halfed = int.class == Integer ? int /2 : nil
+  end
+  
+
